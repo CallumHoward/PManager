6feb040483d3980707f24a496f3b4f77c10feed2
CallumHoward <callumh@cse.unsw.edu.au>
2019-05-04 12:08:52 +1000

    trying to get fonts working correctly WIP

--- src/PManagerApp.cpp
+++ src/PManagerApp.cpp
@@ -16,8 +16,13 @@ public:
     void mouseDown(MouseEvent event) override;
     void update() override;
     void draw() override;
+    static void prepareSettings(PManagerApp::Settings *settings) {
+        settings->setHighDensityDisplayEnabled();
+    }
 
 private:
+    static const bool sHiDpi = true;
+    ImFont* font1;
     float mNum = 0.0;
     boost::container::flat_map<string, float> mParams;
 };
@@ -29,12 +34,31 @@ void PManagerApp::setup() {
     mParams["Buz"] = 0.0f;
 
     // create the renderer and initialize the ui resources.
-    ui::initialize(); //TODO ui::Options change font
+    const auto fontName = "Roboto-Medium.ttf";
+    auto ttfFont = getAssetPath(fontName);
+
+    const auto options = ui::Options()
+        .font(ttfFont, 24)
+        //.fontGlyphRanges(fontName, ui::GetIO().Fonts->GetGlyphRangesDefault())
+        .windowRounding(0.0f);
+    ui::initialize(options);
+
+    //auto io = ui::GetIO();
+    ////font1 = io.Fonts->AddFontFromFileTTF(ttfFont.c_str(), 24);
+    ////io.Fonts->GetTexDataAsRGBA32();
+    //font1 = io.Fonts->AddFontDefault();
+    //assert(font1 != NULL);
+    //ui::PushFont(font1);
 }
 
 void PManagerApp::mouseDown(MouseEvent event) {}
 
 void PManagerApp::update() {
+
+    ui::SetNextWindowPos(ivec2{0, 0});
+    ui::SetNextWindowSize(getWindowSize() * (sHiDpi ? 2 : 1));
+    ui::ScopedWindow window{"Parameter Manager", ImGuiWindowFlags_NoTitleBar};
+
     // slider
     ui::SliderFloat("Number", &mNum, 0.01f, 0.0f);
     // min max
@@ -52,14 +76,14 @@ void PManagerApp::update() {
     for (auto& param : mParams) {
         auto pKey = param.first;
         auto pValue = param.second;
-        ui::RadioButton(std::to_string(i).c_str(), &selected, i);
+        auto id = "##" + std::to_string(i);  // after ## is not visible
+        ui::RadioButton(id.c_str(), &selected, i);
         ui::SameLine();
         ui::SliderFloat(pKey.c_str(), &pValue, 0.0f, 1.0f);
         ++i;
     }
-
 }
 
 void PManagerApp::draw() { gl::clear(Color(0, 0, 0)); }
 
-CINDER_APP(PManagerApp, RendererGl)
+CINDER_APP(PManagerApp, RendererGl, PManagerApp::prepareSettings)
